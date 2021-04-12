b9e1fa0545cf543c0dddb014e8e9b1ef85c14e8d
Sulemaan khan <51238352+Sulemaankhan@users.noreply.github.com>
2021-04-12 09:18:19 +0530

    initial commit
    thread producer and consumer concept

---
+++ thread/Producer.java
@@ -0,0 +1,27 @@
+package com.hcl.thread;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Producer implements Runnable {
+
+    private final BlockingQueue sharedQueue;
+
+    public Producer(BlockingQueue sharedQueue) {
+        this.sharedQueue = sharedQueue;
+    }
+
+    @Override
+    public void run() {
+        for(int i=0; i<10; i++){
+            try {
+                System.out.println("Produced: " + i);
+                sharedQueue.put(i);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
